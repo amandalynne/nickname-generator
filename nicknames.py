5bd061a9b2437024fa4e6b013dac367171e5bd57
temblorosa <amandalynne.p@gmail.com>
2015-09-21 22:59:27 -0400

    made it a class

--- nicknames.py
+++ nicknames.py
@@ -4,24 +4,31 @@ from collections import defaultdict
 
 """Rhyming nickname generator"""
 
-def setup_name_dict():
-    """Create a dictionary of names and pronunciations"""
-    names = set(nltk.corpus.names.words())
-    pron_key = nltk.corpus.cmudict.entries()
-    name_dict = defaultdict(list)
-    for name, pron in pron_key:
-        if name in names:
-            name_dict[name] = pron 
-    return name_dict
-    
-def gen_nickname(name):
-    return "hi" 
+
+class NicknameGenerator:
+    def __init__(self):
+        self.name_dict = self.create_name_dict()
+
+    def create_name_dict(self):
+        """Create a dictionary of names and pronunciations"""
+        names = set(nltk.corpus.names.words())
+        pron_key = nltk.corpus.cmudict.entries()
+        name_dict = defaultdict(list)
+        for name, pron in pron_key:
+            if name in names:
+                name_dict[name] = pron 
+        return name_dict
+        
+    def gen_nickname(self, name):
+        pronunciation = self.name_dict[name]
 
 
 def main():
+    ng = NicknameGenerator()
+    
     while True: 
         name = raw_input("Enter a name: ")
-        nickname = gen_nickname(name)
+        nickname = ng.gen_nickname(name)
         print("Your nickname is {}".format(nickname))
         again = raw_input("Go again? ")
         if again.lower() in ['no','n']:
