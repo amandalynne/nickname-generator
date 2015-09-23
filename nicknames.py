829345c487c3c3d80077d0bba0b7ed2ce2fa7e1c
temblorosa <amandalynne.p@gmail.com>
2015-09-22 23:46:43 -0400

    blah

--- nicknames.py
+++ nicknames.py
@@ -17,30 +17,38 @@ class NicknameGenerator:
         name_dict = defaultdict(list)
         for name, pron in self._pron_key:
             if name.title() in self._names:
-                name_dict[name] = pron 
+                name_dict[name.title()] = pron 
         return name_dict
             
     def create_nouns_dict(self):
         nouns_dict = defaultdict(list)
-        for name, pron in self._pron_key:
-            if name.title() not in self._names and nltk.pos_tag([word])=='NN':
-                nouns_dict[name] = pron
+        for word, pron in self._pron_key:
+            if word.title() not in self._names and nltk.pos_tag([word])=='NN':
+                nouns_dict[word] = pron
         return nouns_dict
+   
+    # TO DO: pickle nouns; maybe sonority hierarchy? 
 
     def syllable_count(self, phones):
         syllables = 0
+        for phone in phones:
+            print phone
+            if phone[-1].isdigit():
+                syllables+=1
+        return syllables
         
     def last_syllable(self, phones):
         syllable = []
         for phone in reversed(phones):
-        
+             
+        return syllable
 
     def gen_nickname(self, name):
-        pronunciation = self.name_dict[name]
+        pronunciation = self.name_dict[name.title()]
         candidates = []
-        for word, pron in self.nouns_dict.items():
-            pass  
-    
+        #for word, pron in self.nouns_dict.items():
+        return self.syllable_count(pronunciation)   
+         
 
 
 def main():
