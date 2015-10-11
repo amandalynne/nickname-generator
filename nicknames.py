0bfc45b64361ff3e2ff85c93d87922568a108d42
temblorosa <amandalynne.p@gmail.com>
2015-10-11 19:40:22 -0400

    added name, adj, and verb dicts

--- nicknames.py
+++ nicknames.py
@@ -13,17 +13,25 @@ class NicknameGenerator:
         self._pron_key = nltk.corpus.cmudict.entries()
         self._vowels = set(['A','E','I','O','U'])
         
-        self.name_dict = self.create_name_dict()
+        self.names_dict = self.create_names_dict()
         self.nouns_dict = self.create_nouns_dict()
+        self.verbs_dict = self.create_verbs_dict()
+        self.adjs_dict = self.create_adjs_dict()
 
 
-    def create_name_dict(self):
+    def create_names_dict(self):
         """Create a dictionary of names and pronunciations"""
-        name_dict = defaultdict(list)
-        for name, pron in self._pron_key:
-            if name.title() in self._names:
-                name_dict[name.title()] = pron 
-        return name_dict
+        if os.path.isfile('names.json'):
+            with open('names.json','rb') as inf:
+                names_dict = json.load(inf)
+        else:
+            names_dict = defaultdict(list)
+            for name, pron in self._pron_key:
+                if name.title() in self._names:
+                    names_dict[name.title()] = pron
+            with open('names.json', 'wb') as outf:
+                json.dump(names_dict, outf)
+        return names_dict
 
             
     def create_nouns_dict(self):
@@ -34,12 +42,48 @@ class NicknameGenerator:
         else:
             nouns_dict = defaultdict(list)
             for word, pron in self._pron_key:
-                if (word.title() not in self._names and nltk.pos_tag([word])[0][1]=='NN'):
+                pos_tag = nltk.pos_tag([word])[0][1]
+                if (word.title() not in self._names and pos_tag == 'NN'):
                     nouns_dict[word] = pron
             with open('nouns.json','wb') as outf:
                 json.dump(nouns_dict, outf)  
         return nouns_dict
-  
+
+
+    def create_verbs_dict(self):
+        """Create dictionary of gerunds and pronunciations,
+           but change pronunciation for better "rhyme" """
+        if os.path.isfile('verbs.json'):
+            with open('verbs.json','rb') as inf:
+                verbs_dict = json.load(inf)
+        else:
+            verbs_dict = defaultdict(list)
+            for word, pron in self._pron_key:
+                pos_tag = nltk.pos_tag([word])[0][1]
+                if (word.title() not in self._names and pos_tag == 'VBG'):
+                    pron[-1] = 'N'
+                    verbs_dict[word] = pron
+            with open('verbs.json','wb') as outf:
+                json.dump(verbs_dict, outf)  
+        return verbs_dict
+
+
+    def create_adjs_dict(self):
+        """Create dictionary of adjectives and pronunciations"""
+        if os.path.isfile('adjs.json'):
+            with open('adjs.json','rb') as inf:
+                adjs_dict = json.load(inf)
+        else:
+            adjs_dict = defaultdict(list)
+            for word, pron in self._pron_key:
+                # Is there a way to not run this 133K times
+                test_sent = "I am {}".format(word).split()
+                pos_tag = nltk.pos_tag(test_sent)[2][1]
+                if (word.title() not in self._names and pos_tag == 'JJ'):
+                    adjs_dict[word] = pron
+            with open('adjs.json','wb') as outf:
+                json.dump(adjs_dict, outf)  
+        return adjs_dict
  
     def syllable_count(self, phones):
         syllables = 0
@@ -70,17 +114,23 @@ class NicknameGenerator:
         rhyme_part.reverse()
         return rhyme_part 
     
-
-    def gen_nickname(self, name):
+    def gen_nickname(self, pos, name):
+        """
+        Pass a POS as well to choose from adj, verb, or noun
+        nickname
+        """
         name = name.title()
         if name not in self._names:
             return None 
-        pronunciation = self.name_dict[name]
+        pronunciation = self.names_dict[name]
         candidates = []
         name_rhyme_part = self.rhyme(pronunciation)
-        for word, pron in self.nouns_dict.items():
-            noun_rhyme_part = self.rhyme(pron)
-            if name_rhyme_part == noun_rhyme_part:
+            
+        
+
+        for word, pron in d.items():
+            word_rhyme_part = self.rhyme(pron)
+            if name_rhyme_part == word_rhyme_part:
                 candidates.append(word)           
         if not candidates:
             return None
